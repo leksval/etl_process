5fa8eb3841ea425f8d0eb280c558f995f6aa7aa2
leksval <s200400@wizard.uek.krakow.pl>
2019-11-24 14:57:55 +0100

    scraping director and cast

--- etl_process/extract_part/main.py
+++ etl_process/extract_part/main.py
@@ -1,14 +1,12 @@
 from bs4 import BeautifulSoup
 from datetime import date
 import requests
-# import random
 
 
 def get_most_popular_movies(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
     movies = soup.find_all("td", class_="titleColumn", limit=10)
-    # random.shuffle(movies)
     return movies
 
 
@@ -21,15 +19,18 @@ def get_movie_summary(url):
 def get_movie_title_and_year(url):
     movie_page = requests.get(url)
     soup = BeautifulSoup(movie_page.text, 'html.parser')
+
     original_title = soup.find("div", class_="originalTitle")
+    # w h1 podaje tytuł z dopiskiem original title w nawiasie więc przycinam
     title = soup.find("h1").text.split('(')[0]
     year = soup.find("span", id="titleYear").text
-    return title if original_title is None else original_title.text.split('(')[0], year
+    director = soup.find("div", class_="credit_summary_item").text
+    # Pierwsza wartość w cast to napis cast ovwrview
+    cast = [review.text for review in soup.find_all('tr', limit=5)]
+    return title if original_title is None else original_title.text.split('(')[0], year, director, cast
 
 
 def get_movies_urls(movie):
-    # movie_title = movie.a.contents[0]
-    # movie_year = movie.span.contents[0]
     movie_url = 'http://www.imdb.com' + movie.a['href']
     movie_reviews_url = movie_url + 'reviews'
     return movie_url, movie_reviews_url
@@ -47,22 +48,25 @@ def most_popular_movies_on_imdb():
     print("Today's date:", today)
     print("--------------------------------------------")
     for movie in get_most_popular_movies('https://www.imdb.com/chart/moviemeter'):
+
         movie_url, movie_reviews_url = get_movies_urls(movie)
+
         movie_summary = get_movie_summary(movie_url)
-        # print(movie_title, movie_year)
+        reviews = get_movie_reviews(movie_reviews_url)
+        title, year, director, cast = get_movie_title_and_year(movie_url)
 
-        # moja funkcja pozyskiwnia tytułu i roku
-        title, year = get_movie_title_and_year(movie_url)
         print(title, year)
+        print(director)
+        print(cast)
+        print("_________________________________________")
+
         print(movie_summary)
+        print("_________________________________________")
 
-        print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
-
-        for rev in get_movie_reviews(movie_reviews_url):
-            print(rev)
-            print("++++++++++++++++++++++++++++++++++++++++++++")
-
-        print("--------------------------------------------")
+  #      for rev in reviews:
+  #          print(rev)
+  #          print("++++++++++++++++++++++++++++++++++++++++++++")
+  #      print("--------------------------------------------")
 
 
 if __name__ == '__main__':

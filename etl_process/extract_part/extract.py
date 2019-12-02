b8a750fb6db8300d5bbb79c0241b3f44fd61897e
leksval <leksval@github.com>
2019-12-03 00:57:33 +0100

    Docstrings addet to extract.py

--- etl_process/extract_part/extract.py
+++ etl_process/extract_part/extract.py
@@ -10,6 +10,7 @@ IMDB_CHART_LINK = 'https://www.imdb.com/chart/moviemeter'
 
 
 def get_most_popular_movies(url):
+    '''Return titles of most popular movies, needed to generate urls with details and reviews.'''
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
     movies = soup.find_all("td", class_="titleColumn", limit=MOVIE_QUANTITY)
@@ -17,12 +18,14 @@ def get_most_popular_movies(url):
 
 
 def get_movie_summary(url):
+    ''' Return movie summary.'''
     movie_page = requests.get(url)
     soup = BeautifulSoup(movie_page.text, 'html.parser')
     return soup.find("div", class_="summary_text").contents[0].strip()
 
 
 def get_movie_title_and_year(url):
+    '''Return movie title and year.'''
     movie_page = requests.get(url)
     soup = BeautifulSoup(movie_page.text, 'html.parser')
 
@@ -38,17 +41,21 @@ def get_movie_title_and_year(url):
 
 
 def get_movie_urls(movie):
+    '''Return urls of movie details and reviews.'''
     movie_url = 'http://www.imdb.com' + movie.a['href']
     movie_reviews_url = movie_url + 'reviews'
     return movie_url, movie_reviews_url
 
 
 def get_movie_reviews(movie_reviews_url):
+    '''Return movie reviews.'''
     reviews_page = requests.get(movie_reviews_url)
     soup = BeautifulSoup(reviews_page.text, 'html.parser')
     reviews = [review.text for review in soup.find_all('div', class_="text", limit=REVIEWS_QUANTITY)]
     return reviews
 
+'''
+Printing functions (terminal):
 
 def print_results_in_terminal(title, year, director, cast, movie_summary, reviews):
     print(title, year)
@@ -60,14 +67,14 @@ def print_results_in_terminal(title, year, director, cast, movie_summary, review
     print(*reviews, sep='\n ++++++++++++++++++++ \n')
     print("-----------------------------------------")
 
-
 def print_today_date():
     today = date.today()
     print("Today's date:", today)
     print("------------------------")
-
+'''
 
 def dig_out_movie_details(movie):
+    '''Returns movie details and reviews.'''
     movie_url, movie_reviews_url = get_movie_urls(movie)
 
     movie_summary = get_movie_summary(movie_url)
@@ -81,13 +88,12 @@ def dig_out_movie_details(movie):
 
 
 def extract_movies_from_chart_from_imdb():
+    '''Main extracting function which returns csv file.'''
     df = pd.DataFrame(columns=['Title', 'Year', 'Director', 'Cast', 'Movie summary', 'Reviews'])
 
     for movie in get_most_popular_movies(IMDB_CHART_LINK):
         df = df.append(dig_out_movie_details(movie), ignore_index=True)
 
-    print_today_date()
-    print(df)
     df.to_csv("output.csv", index=False)
 
 
